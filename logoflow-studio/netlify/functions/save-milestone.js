9c1cc25310dd4cfe13b15e1bc2743dba33600d7e
Wis-code <chiemeziem60@gmail.com>
2026-05-23 12:45:22 +0100

    Update save-milestone.js

--- logoflow-studio/netlify/functions/save-milestone.js
+++ logoflow-studio/netlify/functions/save-milestone.js
@@ -16,7 +16,7 @@ exports.handler = async (event, context) => {
   try {
     const payload = JSON.parse(event.body);
 
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbyqmP5hW4hCbfWxGgiHW4fFK6rYBD_fCIo6fUO6R2hRVTedn2as4H6yjY2LFNanNKl2/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbxXMXphQtjqtE-ovtXbqOsSl8Uqtz2e0JxHPlKX080Jl3wcLYlH8UX3CAaRmkeSZB4A/exec';
 
     const response = await fetch(scriptUrl, {
       method: 'POST',
